e2bb7ceda68f09c0b0ac749b186383e1b8a06d79
Iqbal Fasri <iqbalx64@gmail.com>
2019-06-11 15:11:32 +0700

    Test bug solve

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,22 @@
  */
 
 // You can delete this file if you're not using it
+export const onPreRenderHTML = ({
+  getHeadComponents,
+  pathname,
+  replaceHeadComponents,
+}) => {
+  if (pathname !== `/offline-plugin-app-shell-fallback/`) return;
+  const headComponents = getHeadComponents();
+  const filteredHeadComponents = headComponents.filter(
+    ({ type, props }) =>
+      !(
+        type === `link` &&
+        props.as === `fetch` &&
+        props.rel === `preload` &&
+        props.href.startsWith(`/static/id`)
+      )
+  );
+
+  replaceHeadComponents(filteredHeadComponents);
+};
